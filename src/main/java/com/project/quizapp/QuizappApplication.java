09b7516f1408f3a753802968d7b17baec68ae338
AVIRAL KUMAR <aviralkumar2003@gmail.com>
2024-04-25 22:59:28 +0530

    commit2

--- src/main/java/com/project/quizapp/QuizappApplication.java
+++ src/main/java/com/project/quizapp/QuizappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.project.model")
+@EntityScan(basePackages = "com.project.quizapp.model")
 public class QuizappApplication {
 
 	public static void main(String[] args) {
